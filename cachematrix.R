2064284e0731025642219ca0796e33d62994cd33
Eric1ycWong <b01303010@ntu.edu.tw>
2016-03-06 10:49:46 +0800

    Added files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,36 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        i<<-NULL
+        get<-function() x
+        set<-function(y){
+                x<<-y
+                i<<-NULL
+        }
+        setinverse<-function(inverse) i<<-inverse
+        getinverse<-function() i
+        list(get=get, set=set, getinverse=getinverse, setinverse=setinverse)
+        
 
 }
 
-
-## Write a short comment describing this function
+##this function assign the non-existed matrix to the variable x
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i<<-x$getinverse()
+        if(!is.null(i)){
+                message("from cache we got")
+                return(i)
+        }
+        ##here, we exam whether the getinverse already exists its value
+        ##if is case, then we just simply print back the previous value
+        else{
+                matrix<-x$get()
+                i<<-solve(matrix)
+                x$setinverse(i)
+                i
+        }
+        ##this subset first assign the element "matrix" with proper value
+        ##then solve the inverse and assign the result into setinverse
+
 }
